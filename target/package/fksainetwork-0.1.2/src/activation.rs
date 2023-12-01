5fcb7b0fe5bc378dcff521d51acbb830f9362187
Felix1G <87594136+Felix1G@users.noreply.github.com>
2023-12-01 23:17:22 +0800

    v0.1.2 publish

---
+++ target/package/fksainetwork-0.1.2/src/activation.rs
@@ -0,0 +1,58 @@
+use crate::util;
+
+trait Activation {
+    fn activate(&self, _value: f32) -> f32 { 0.0 }
+    fn derivative(&self, _value: f32) -> f32 { 0.0 }
+}
+
+struct LinearActivation;
+
+struct SigmoidActivation;
+
+struct ReLUActivation;
+
+const LINEAR: LinearActivation = LinearActivation;
+const SIGMOID: SigmoidActivation = SigmoidActivation;
+const RELU: ReLUActivation = ReLUActivation;
+
+pub fn activate(mode: usize, value: f32) -> f32 {
+    return match mode {
+        0 => LINEAR.activate(value),
+        1 => SIGMOID.activate(value),
+        2 => RELU.activate(value),
+        _ => LINEAR.activate(value)
+    }
+}
+
+pub fn derivative(mode: usize, value: f32) -> f32 {
+    return match mode {
+        0 => LINEAR.derivative(value),
+        1 => SIGMOID.derivative(value),
+        2 => RELU.derivative(value),
+        _ => LINEAR.derivative(value)
+    }
+}
+
+impl Activation for LinearActivation {
+    fn activate(&self, value: f32) -> f32 { value }
+    fn derivative(&self, _value: f32) -> f32 { 1.0 }
+}
+
+impl Activation for SigmoidActivation {
+    fn activate(&self, value: f32) -> f32 { 1.0 / (1.0 + f32::exp(-value)) }
+    fn derivative(&self, value: f32) -> f32 {
+        let result = self.activate(value);
+        result * (1.0 - result)
+    }
+}
+
+impl Activation for ReLUActivation {
+    fn activate(&self, value: f32) -> f32 { util::max(0.0, value) }
+    fn derivative(&self, value: f32) -> f32 {
+        if value <= 0.0 {
+            0.0
+        } else {
+            1.0
+        }
+    }
+}
