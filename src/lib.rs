66c4fe51fe112905ee6d2f4958dde88056de4c61
Felix1G <87594136+Felix1G@users.noreply.github.com>
2023-12-01 13:33:04 +0800

    Documentation Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,81 @@
-/*
+mod activation;
+mod neuron;
+mod tests;
+mod util;
+
+/**
 * -- Documentation --
+*
 * This Neural Network is coded and created by Felix K.S
 * (used to be in C++, ported to Rust)
 *
 * The current activation functions are as follows: (index) [function] [derivative function]
 * - Linear (0) [value] [1.0]
 * - Sigmoid (1) [value / (1 + e^(-value))] [f(value) * (1.0 - f(value))]
+* - ReLU (2) [max(0, value)] [if value <= 0.0 ? 0.0 : 1.0]
 *
 * The current methods of learning are as follows:
 * - Mean Squared Error (MSE) using Back Propogation (BPG)
-*		Equation of the output bias gradient (dE/db) is the error term of the output using the error function (E)
+*		    Equation of the output bias gradient (dE/db) is the error term of the output using the error function (E)
 *
-*		εk = Σ [ (ak - tk) * (gk'(zk)) ] where
-*		tk = expected value of the output neuron;
-*		ak = gk(zk) = Neuron::result;
-*		gk = Activation::activate
-*		gk` = Activation::activate_derivative;
-*		zk = Neuron::value;
-*
-*
-*		Equation of the output weight gradient is the error term multipled with
-*		the previous neuron result associated with that weight (dE/dwj)
-*
-*		dE/dwj = εk * aj where
-*		εk = the error term of the output neuron;
-*		wj = the weight of which its gradient is to be calculated;
-*		aj = the result of the previous neuron associated with wj;
-*
-*		------------------
-*
-*		Equation of the hidden layer bias is the utilization of a recursion where
-*		all neurons that are connected with this hidden neuron are taken onto account
-*		during the calculation of its gradient. (dE/dbj)
-*
-*		An error term of j is then created.
-*
-*		εj = [ gj'(zj) ][ Σ (εk * wjk) ] where
-*		gj' = Activation::activate_derivative;
-*		zj = Neuron::value;
-*		εk = error term of the output neuron;
-*		wjk = the weight of the next neuron associated with this hidden neuron;
+*		    εk = Σ [ (ak - tk) * (gk'(zk)) ] where
+*		    tk = expected value of the output neuron;
+*		    ak = gk(zk) = Neuron::result;
+*		    gk = Activation::activate
+*		    gk` = Activation::activate_derivative;
+*		    zk = Neuron::value;
 *
 *
-*		Hence, the weight gradient can be calculated using the same way as above. (dE/dwij)
+*		    Equation of the output weight gradient is the error term multipled with
+*		    the previous neuron result associated with that weight (dE/dwj)
 *
-*		dE/dwij = εj * ai where
-*		wij = the weight of which its gradient is to be calculated;
-*		ai = the result of the previous neuron;
-*		εj = the error term of this hidden layer;
+*		    dE/dwj = εk * aj where
+*		    εk = the error term of the output neuron;
+*		    wj = the weight of which its gradient is to be calculated;
+*		    aj = the result of the previous neuron associated with wj;
 *
-*		------------------
+*		    ------------------
 *
-*		For deeper neural networks, the error term is then plugged in by recursion.
-*		εj = [ gj'(zj) ][ Σ (εk * wjk) ];
-*		εi = [ gj'(zi) ][ Σ (εj * wij) ];
-*		εh = [ gj'(zh) ][ Σ (εi * whi) ];
-*		and so on.
+*		    Equation of the hidden layer bias is the utilization of a recursion where
+*		    all neurons that are connected with this hidden neuron are taken onto account
+*		    during the calculation of its gradient. (dE/dbj)
+*
+*		    An error term of j is then created.
+*
+*		    εj = [ gj'(zj) ][ Σ (εk * wjk) ] where
+*		    gj' = Activation::activate_derivative;
+*		    zj = Neuron::value;
+*		    εk = error term of the output neuron;
+*		    wjk = the weight of the next neuron associated with this hidden neuron;
+*
+*		    Hence, the weight gradient can be calculated using the same way as above. (dE/dwij)
+*
+*		    dE/dwij = εj * ai where
+*		    wij = the weight of which its gradient is to be calculated;
+*		    ai = the result of the previous neuron;
+*		    εj = the error term of this hidden layer;
+*
+*		    ------------------
+*
+*		    For deeper neural networks, the error term is then plugged in by recursion.
+*		    εj = [ gj'(zj) ][ Σ (εk * wjk) ];
+*		    εi = [ gj'(zi) ][ Σ (εj * wij) ];
+*		    εh = [ gj'(zh) ][ Σ (εi * whi) ];
+*		    and so on.
 *
 * The Network is saved using the following serialization format:
-*		[number of layers] [number of neurons of layer 0] [number of neurons of layer 1] [number of neurons of layer n]
-*		[activation of layer 0] [activation of layer 1] [activation of layer n]
-*		[bias of neuron 0] [weight 0 of neuron 0] [weight 1 of neuron 0] [weight n of neuron 0] //begins on the first hidden layer
-*		[bias of neuron 1] [weight 0 of neuron 1] [weight 1 of neuron 1] [weight n of neuron 1]
-*		...
-*		[bias of neuron n] [weight 0 of neuron n] [weight 1 of neuron n] [weight n of neuron n]
 *
-*		Neurons are then put accordingly to its index to its respective layer.
-*		The first neurons are put in the first hidden layer, then the following
-*		is put in the next layer, and so on.
+*		    [number of layers] [number of neurons of layer 0] [number of neurons of layer 1] [number of neurons of layer n]
+*		    [activation of layer 0] [activation of layer 1] [activation of layer n]
+*		    [bias of neuron 0] [weight 0 of neuron 0] [weight 1 of neuron 0] [weight n of neuron 0] //begins on the first hidden layer
+*		    [bias of neuron 1] [weight 0 of neuron 1] [weight 1 of neuron 1] [weight n of neuron 1]
+*		    ...
+*		    [bias of neuron n] [weight 0 of neuron n] [weight 1 of neuron n] [weight n of neuron n]
 *
-*
-* Note to programmer:
-* - Inputs of the neurons and activations should not be deleted as it is deleted by Network::~Network().
-* - The float array of Network::output() should be deleted after being used.
+*		    Neurons are then put accordingly to its index to its respective layer.
+*		    The first neurons are put in the first hidden layer, then the following
+*		    is put in the next layer, and so on.
 */
-mod activation;
-mod neuron;
-mod tests;
-mod util;
-
 pub mod network {
     use std::fmt::Formatter;
     use std::fs::File;
@@ -95,6 +92,19 @@ pub mod network {
     }
 
     impl Network {
+        /**
+        Create a new Network.
+
+        Layers are provided in this format:
+
+                [layer 0 size, layer 1 size, layer n size].
+
+        Activations are provided in this format:
+
+                [layer 0 activation (although not used), layer 1 activation, layer n activation]
+
+        Indices are provided at the documentation of Network.
+        */
         pub fn new(layers: &[usize], activations: &[usize]) -> Self {
             let mut neurons = Vec::<Vec::<Neuron>>::new();
 
--- target/package/fksainetwork-0.1.0/target/debug/deps/cfg_if-656685f8f63d488b.d
+++ target/package/fksainetwork-0.1.0/target/debug/deps/cfg_if-656685f8f63d488b.d
@@ -1,7 +1,7 @@
-C:\Users\ACER\RustroverProjects\fksainetwork\target\debug\deps\libcfg_if-656685f8f63d488b.rmeta: C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+C:\Users\ACER\OneDrive\Documents\GitHub\fksainetwork\target\package\fksainetwork-0.1.0\target\debug\deps\libcfg_if-656685f8f63d488b.rmeta: C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
 
-C:\Users\ACER\RustroverProjects\fksainetwork\target\debug\deps\libcfg_if-656685f8f63d488b.rlib: C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+C:\Users\ACER\OneDrive\Documents\GitHub\fksainetwork\target\package\fksainetwork-0.1.0\target\debug\deps\libcfg_if-656685f8f63d488b.rlib: C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
 
-C:\Users\ACER\RustroverProjects\fksainetwork\target\debug\deps\cfg_if-656685f8f63d488b.d: C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+C:\Users\ACER\OneDrive\Documents\GitHub\fksainetwork\target\package\fksainetwork-0.1.0\target\debug\deps\cfg_if-656685f8f63d488b.d: C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
 
 C:\Users\ACER\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
