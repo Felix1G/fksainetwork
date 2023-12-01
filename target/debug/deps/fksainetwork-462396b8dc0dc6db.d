5a03be3f31736e68b9fcdd36de29a0ba389b1fb9
Felix1G <87594136+Felix1G@users.noreply.github.com>
2023-12-01 23:15:15 +0800

    Slight Changes for v0.1.2

---
+++ target/debug/deps/fksainetwork-462396b8dc0dc6db.d
@@ -0,0 +1,10 @@
+C:\Users\ACER\OneDrive\Documents\GitHub\fksainetwork\target\debug\deps\fksainetwork-462396b8dc0dc6db.exe: src\lib.rs src\activation.rs src\neuron.rs src\tests.rs src\tests\alpha_3x5_test.rs src\util.rs
+
+C:\Users\ACER\OneDrive\Documents\GitHub\fksainetwork\target\debug\deps\fksainetwork-462396b8dc0dc6db.d: src\lib.rs src\activation.rs src\neuron.rs src\tests.rs src\tests\alpha_3x5_test.rs src\util.rs
+
+src\lib.rs:
+src\activation.rs:
+src\neuron.rs:
+src\tests.rs:
+src\tests\alpha_3x5_test.rs:
+src\util.rs:
--- target/generated/bincode/Neuron_Encode.rs
+++ target/generated/bincode/Neuron_Encode.rs
@@ -1,1 +1,15 @@
-impl :: bincode :: Encode for Neuron {fn encode < __E : :: bincode :: enc :: Encoder > (& self , encoder : & mut __E) ->core :: result :: Result < () , :: bincode :: error :: EncodeError > {:: bincode :: Encode :: encode (& self . weights_temp , encoder) ?; :: bincode :: Encode :: encode (& self . weights , encoder) ?; :: bincode :: Encode :: encode (& self . bias_temp , encoder) ?; :: bincode :: Encode :: encode (& self . bias , encoder) ?; :: bincode :: Encode :: encode (& self . value , encoder) ?; :: bincode :: Encode :: encode (& self . result , encoder) ?; :: bincode :: Encode :: encode (& self . activation , encoder) ?; Ok (())}}
+impl :: bincode :: Encode for Neuron
+{
+    fn encode < __E : :: bincode :: enc :: Encoder >
+    (& self, encoder : & mut __E) -> core :: result :: Result < (), :: bincode
+    :: error :: EncodeError >
+    {
+        :: bincode :: Encode :: encode(& self.weights_temp, encoder) ? ; ::
+        bincode :: Encode :: encode(& self.weights, encoder) ? ; :: bincode ::
+        Encode :: encode(& self.bias_temp, encoder) ? ; :: bincode :: Encode
+        :: encode(& self.bias, encoder) ? ; :: bincode :: Encode ::
+        encode(& self.value, encoder) ? ; :: bincode :: Encode ::
+        encode(& self.result, encoder) ? ; :: bincode :: Encode ::
+        encode(& self.activation, encoder) ? ; Ok(())
+    }
+}
